61a11e69c43384fe2fd273173396956128df3fee
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 12:23:23 -0800

    word_count_dict_iteritems

--- wordcount.py
+++ wordcount.py
@@ -1,10 +1,23 @@
 poem = open("test.txt")
 
+twain = open("twain.txt")
+
 word_count = {}
 
 
-for line in poem:
+# for line in poem:
+#     split_line = line.rstrip().split(" ")
+#     for word in split_line:
+#         word_count[word] = word_count.get(word, 0) + 1
+
+# for word, count in word_count.items():
+#     print word + " " + str(count)
+
+
+for line in twain:
     split_line = line.rstrip().split(" ")
     for word in split_line:
         word_count[word] = word_count.get(word, 0) + 1
-print word_count
+
+for word, count in word_count.iteritems():
+    print word + " " + str(count)
