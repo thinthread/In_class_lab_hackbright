d69cff8dab9d914632df4bb8b5fe000d7339627d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 12:39:01 -0800

    word_count_dict_in_functions

--- wordcount.py
+++ wordcount.py
@@ -2,22 +2,32 @@ poem = open("test.txt")
 
 twain = open("twain.txt")
 
-word_count = {}
 
 
-# for line in poem:
-#     split_line = line.rstrip().split(" ")
-#     for word in split_line:
-#         word_count[word] = word_count.get(word, 0) + 1
+# def create_test_dict(poem):
+#     """Create a word count for poem"""
 
-# for word, count in word_count.items():
-#     print word + " " + str(count)
+#     for line in poem:
+#         split_line = line.rstrip().split(" ")
+#         for word in split_line:
+#             word_count[word] = word_count.get(word, 0) + 1
+
+#     for word, count in word_count.items():
+#         print word + " " + str(count)
+
+# create_test_dict(poem)
 
 
-for line in twain:
-    split_line = line.rstrip().split(" ")
-    for word in split_line:
-        word_count[word] = word_count.get(word, 0) + 1
+def create_twain_dict(twain):
+    '''creat a word counter, save words to dictionary'''
 
-for word, count in word_count.iteritems():
-    print word + " " + str(count)
+    word_count = {}
+    for line in twain:
+        split_line = line.rstrip().split()
+        for word in split_line:
+            word_count[word] = word_count.get(word, 0) + 1
+
+    for word, count in word_count.iteritems():
+        print word + " " + str(count)
+
+create_twain_dict(twain)
